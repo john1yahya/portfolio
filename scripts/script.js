1c4fda93f4952c702a01cbeca81cfbb4c9481afe
john1yahya <yahya.hoummani.50@edu.uiz.ac.ma>
2025-08-11 18:46:21 +0100

    sadhh

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,22 @@
-document.querySelector('.menu').addEventListener('click', () => {
+
+
+
+const excludeButton = document.querySelector('.menu-x'); // Replace with your button's selector
+
+document.addEventListener('click', function(event) {
+  // Check if the clicked element is the button you want to exclude
+  if (event.target === excludeButton) {
+    document.querySelector('.menu-x').addEventListener('click', () => {
+    document.querySelector('.mobile-nav')
+    .classList.remove('active');
+    // document.querySelector('.mobile-nav').style.display = 'none';
+    });
+    
+  }else{
+    document.querySelector('.menu').addEventListener('click', () => {
     document.querySelector('.mobile-nav')
     .classList.add('active');
     document.querySelector('.mobile-nav').style.display = 'flex';
 });
-
-document.querySelector('.menu-x').addEventListener('click', () => {
-    document.querySelector('.mobile-nav')
-    .classList.remove('active');
-    // document.querySelector('.mobile-nav').style.display = 'flex';
-
-    // document.querySelector('.mobile-nav').style.display = 'none';
-});
+  }
+});  
