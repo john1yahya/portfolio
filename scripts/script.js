08ab33fb0eb497b5da710c44024700d12fef5d94
john1yahya <yahya.hoummani.50@edu.uiz.ac.ma>
2025-08-11 18:22:41 +0100

    change mobile humb

--- scripts/script.js
+++ scripts/script.js
@@ -7,5 +7,7 @@ document.querySelector('.menu').addEventListener('click', () => {
 document.querySelector('.menu-x').addEventListener('click', () => {
     document.querySelector('.mobile-nav')
     .classList.remove('active');
+    // document.querySelector('.mobile-nav').style.display = 'flex';
+
     // document.querySelector('.mobile-nav').style.display = 'none';
 });
