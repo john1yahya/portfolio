9307b5dd53e5f925051ccb5b5c5da635e3737531
john1yahya <yahya.hoummani.50@edu.uiz.ac.ma>
2025-08-14 20:58:48 +0100

    fixin overflow

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,20 @@
 document.querySelector('.menu').addEventListener('click', () => {
     document.querySelector('.mobile-nav')
     .classList.add('active');
-    document.querySelector('.mobile-nav').style.display = 'flex';
+    // document.querySelector('.mobile-nav').style.display = 'flex';
     document.body.style.overflow = 'hidden';
 });
 
 document.querySelector('.menu-x').addEventListener('click', () => {
     document.querySelector('.mobile-nav')
     .classList.remove('active');
-    document.querySelector('.mobile-nav').style.display = 'none';
-    document.body.style.overflow = 'scroll';
+    // document.querySelector('.mobile-nav').style.display = 'none';
+    document.body.style.overflowY = 'scroll';
 });
 document.querySelectorAll('.menu-element').forEach((element) => {
     element.addEventListener('click', () => {
     document.querySelector('.mobile-nav')
         .classList.remove('active');
-    document.querySelector('.mobile-nav').style.display = 'none';
-    document.body.style.overflow = 'scroll';
+    document.body.style.overflowY = 'scroll';
     } )
 })
