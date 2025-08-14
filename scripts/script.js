af972dea6c54a5a91e31517268aa58422b0bad81
john1yahya <yahya.hoummani.50@edu.uiz.ac.ma>
2025-08-14 17:55:27 +0100

    scrol behavior

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,12 @@ document.querySelector('.menu').addEventListener('click', () => {
     document.querySelector('.mobile-nav')
     .classList.add('active');
     document.querySelector('.mobile-nav').style.display = 'flex';
+    document.body.style.overflow = 'hidden'
 });
 
 document.querySelector('.menu-x').addEventListener('click', () => {
     document.querySelector('.mobile-nav')
     .classList.remove('active');
     document.querySelector('.mobile-nav').style.display = 'none';
-    // document.querySelector('.mobile-nav').style.display = 'none';
+    document.body.style.overflow = 'scroll'
 });
